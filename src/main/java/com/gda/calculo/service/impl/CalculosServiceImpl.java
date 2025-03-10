abd958ddde5b08aef49cea2fdd7a3973ea54850d
Andres Baez <andresxyk@hotmail.com>
2025-03-09 23:10:24 -0600

    modificacion de actualizaciones e insert

--- src/main/java/com/gda/calculo/service/impl/CalculosServiceImpl.java
+++ src/main/java/com/gda/calculo/service/impl/CalculosServiceImpl.java
@@ -3,7 +3,10 @@
  */
 package com.gda.calculo.service.impl;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.sql.DataSource;
 
@@ -43,25 +46,23 @@ public class CalculosServiceImpl extends JdbcDaoSupport implements CalculosServi
 		String var1, strsql;
 		String opc = "";
 		String select = "";
-		
-		if(ntipoprevio.equals("1")){	
+
+		if (ntipoprevio.equals("1")) {
 			select = " consecutivo,consecutivofac,kpaciente,noconvenio,nombrepaciente,codigoestudio,nombreestudio,costo_unitario,miva,mtotal,totalfactura,bloque,fechaorden,sclasificacioncomercial,ufoliofactura,kfactura ";
-			opc = " order by Bloque, "
-					+ "	Consecutivo, "
-					+ "	CodigoEstudio;";
-		} else if(ntipoprevio.equals("2")) {
-		} else if(ntipoprevio.equals("3")) {
+			opc = " order by Bloque, " + "	Consecutivo, " + "	CodigoEstudio;";
+		} else if (ntipoprevio.equals("2")) {
+		} else if (ntipoprevio.equals("3")) {
 			select = " ufoliofactura, kfactura , NoConvenio, CodigoEstudio, count (CodigoEstudio) cantidad, NombreEstudio, sum (costo_unitario) msubtotal, sum(miva) miva , sum(mtotal) mtotal, Bloque ";
 			opc = "  group by ufoliofactura, kfactura , NoConvenio, CodigoEstudio, NombreEstudio, Bloque order by NoConvenio, CodigoEstudio;";
 		}
-		
+
 		List<FuncionFacturacionDto> list = null;
 		if (pCconvenio != 309 && pCconvenio != 310 && pCconvenio != 311 && pCconvenio != 312)
 			var1 = " and tosf.cconvenio = " + (pCconvenio);
 		else
 			var1 = " and tosf.cconvenio in (309,310,311,312)";
 
-		strsql = "select "+ select +" from ("
+		strsql = "select " + select + " from ("
 				+ "select tosf.kordensucursal as Consecutivo, tosf.kordensucursalfac as Consecutivofac,	tp.kpaciente, "
 				+ "tosf.cconvenio as NoConvenio,	"
 				+ "tp.sapellidopaterno||' '||tp.sapellidomaterno||' '||tp.snombre as NombrePaciente,	"
@@ -122,23 +123,24 @@ public class CalculosServiceImpl extends JdbcDaoSupport implements CalculosServi
 				+ " and toesf.kordensucursalfac = tosf.kordensucursalfac "
 				+ "			group by	tosf.kordensucursal, tosf.kordensucursalfac , tp.kpaciente, tosf.uorden,cc.ccliente, tosf.cconvenio ,cc.sconvenio, toesf.cperfil ,toesf.sperfil , ccl.srazonsocial,	"
 				+ "				tp.sapellidopaterno, tp.sapellidomaterno, tp.snombre, tos.sobservacion,tosf.dregistro,tosf.uconsecutivo,tos.dregistro, sclasificacioncomercial	"
-				+ "			ORDER BY 1 ,6)"
-				+ " x " + opc;
-		
-		if(ntipoprevio.equals("1")){	
+				+ "			ORDER BY 1 ,6)" + " x " + opc;
+
+		if (ntipoprevio.equals("1")) {
 			list = this.getJdbcTemplate().query(strsql, new FuncionFacturacionMapper());
-		} else if(ntipoprevio.equals("2")) {
-		} else if(ntipoprevio.equals("3")) {
+		} else if (ntipoprevio.equals("2")) {
+		} else if (ntipoprevio.equals("3")) {
 			list = this.getJdbcTemplate().query(strsql, new CalculoMapper());
 		}
+		log.info("SQL Execute: " + strsql);
 		log.info("Registros obtenidos " + list.size());
 		getDatosAdicionales(pCconvenio, pUconsecutivo, list);
-		
+
 		return list;
 	}
 
 	@Override
-	public List<FuncionFacturacionDto> getDatosAdicionales(Long pCconvenio, String pUconsecutivo, List<FuncionFacturacionDto> lstCalculos) {
+	public List<FuncionFacturacionDto> getDatosAdicionales(Long pCconvenio, String pUconsecutivo,
+			List<FuncionFacturacionDto> lstCalculos) {
 		List<DatosAdicionalesDto> list = null;
 		String var1, var2, var3, strsql;
 
@@ -267,87 +269,87 @@ public class CalculosServiceImpl extends JdbcDaoSupport implements CalculosServi
 		String ssucursalmarca = "";
 
 		if (cmarcasucursal == 1) {
-		    csucursalmarca = 1003;
-		    sseriemarca = "A";
-		    centidadlegalmarca = 1;
-		    ssucursalmarca = "EMPRESAS";
+			csucursalmarca = 1003;
+			sseriemarca = "A";
+			centidadlegalmarca = 1;
+			ssucursalmarca = "EMPRESAS";
 		} else if (cmarcasucursal == 4) {
-		    csucursalmarca = 1012;
-		    sseriemarca = "AZ";
-		    centidadlegalmarca = 5;
-		    ssucursalmarca = "EMPRESAS AZTECA";
+			csucursalmarca = 1012;
+			sseriemarca = "AZ";
+			centidadlegalmarca = 5;
+			ssucursalmarca = "EMPRESAS AZTECA";
 		} else if (cmarcasucursal == 5) {
-		    csucursalmarca = 1013;
-		    sseriemarca = "AS";
-		    centidadlegalmarca = 6;
-		    ssucursalmarca = "EMPRESAS SWISSLAB";
+			csucursalmarca = 1013;
+			sseriemarca = "AS";
+			centidadlegalmarca = 6;
+			ssucursalmarca = "EMPRESAS SWISSLAB";
 		} else if (cmarcasucursal == 15) {
-		    csucursalmarca = 1017;
-		    sseriemarca = "ASL";
-		    centidadlegalmarca = 16;
-		    ssucursalmarca = "EMPRESAS LIACSA";
+			csucursalmarca = 1017;
+			sseriemarca = "ASL";
+			centidadlegalmarca = 16;
+			ssucursalmarca = "EMPRESAS LIACSA";
 		} else if (cmarcasucursal == 19) {
-		    csucursalmarca = 1020;
-		    sseriemarca = "AFN";
-		    centidadlegalmarca = 19;
-		    ssucursalmarca = "EMPRESAS FAMILYLABS NORTE";
+			csucursalmarca = 1020;
+			sseriemarca = "AFN";
+			centidadlegalmarca = 19;
+			ssucursalmarca = "EMPRESAS FAMILYLABS NORTE";
 		} else if (cmarcasucursal == 20) {
-		    csucursalmarca = 1021;
-		    sseriemarca = "AJK";
-		    centidadlegalmarca = 20;
-		    ssucursalmarca = "EMPRESAS EXAKTA";
+			csucursalmarca = 1021;
+			sseriemarca = "AJK";
+			centidadlegalmarca = 20;
+			ssucursalmarca = "EMPRESAS EXAKTA";
 		} else if (cmarcasucursal == 16) {
-		    csucursalmarca = 1024;
-		    sseriemarca = "AIO";
-		    centidadlegalmarca = 18;
-		    ssucursalmarca = "EMPRESAS MOREIRA";
+			csucursalmarca = 1024;
+			sseriemarca = "AIO";
+			centidadlegalmarca = 18;
+			ssucursalmarca = "EMPRESAS MOREIRA";
 		} else if (cmarcasucursal == 21) {
-		    csucursalmarca = 1022;
-		    sseriemarca = "AAS";
-		    centidadlegalmarca = 21;
-		    ssucursalmarca = "EMPRESAS ASESORES DEL SUR";
+			csucursalmarca = 1022;
+			sseriemarca = "AAS";
+			centidadlegalmarca = 21;
+			ssucursalmarca = "EMPRESAS ASESORES DEL SUR";
 		} else if (cmarcasucursal == 22) {
-		    csucursalmarca = 1023;
-		    sseriemarca = "AIM";
-		    centidadlegalmarca = 22;
-		    ssucursalmarca = "EMPRESAS POLAB";
+			csucursalmarca = 1023;
+			sseriemarca = "AIM";
+			centidadlegalmarca = 22;
+			ssucursalmarca = "EMPRESAS POLAB";
 		} else if (cmarcasucursal == 23) {
-		    csucursalmarca = 1027;
-		    sseriemarca = "AIP";
-		    centidadlegalmarca = 23;
-		    ssucursalmarca = "EMPRESAS LISTER";
+			csucursalmarca = 1027;
+			sseriemarca = "AIP";
+			centidadlegalmarca = 23;
+			ssucursalmarca = "EMPRESAS LISTER";
 		} else if (cmarcasucursal == 25) {
-		    csucursalmarca = 1024;
-		    sseriemarca = "AMZ";
-		    centidadlegalmarca = 25;
-		    ssucursalmarca = "EMPRESAS BR";
+			csucursalmarca = 1024;
+			sseriemarca = "AMZ";
+			centidadlegalmarca = 25;
+			ssucursalmarca = "EMPRESAS BR";
 		} else if (cmarcasucursal == 26) {
-		    csucursalmarca = 1025;
-		    sseriemarca = "AIN";
-		    centidadlegalmarca = 26;
-		    ssucursalmarca = "EMPRESAS PROMEDIC";
+			csucursalmarca = 1025;
+			sseriemarca = "AIN";
+			centidadlegalmarca = 26;
+			ssucursalmarca = "EMPRESAS PROMEDIC";
 		} else if (cmarcasucursal == 9) {
-		    csucursalmarca = 1028;
-		    sseriemarca = "AIQ";
-		    centidadlegalmarca = 10;
-		    ssucursalmarca = "EMPRESAS SWISSHOSPITAL";
+			csucursalmarca = 1028;
+			sseriemarca = "AIQ";
+			centidadlegalmarca = 10;
+			ssucursalmarca = "EMPRESAS SWISSHOSPITAL";
 		} else if (cmarcasucursal == 7) {
-		    if (p_razon == 1) {
-		        csucursalmarca = 1014;
-		        sseriemarca = "AJP";
-		        centidadlegalmarca = 7;
-		        ssucursalmarca = "EMPRESAS JENNER PRADO";
-		    } else if (p_razon == 2) {
-		        csucursalmarca = 1015;
-		        sseriemarca = "AJL";
-		        centidadlegalmarca = 8;
-		        ssucursalmarca = "EMPRESAS JENNER LEAN";
-		    } else if (p_razon == 3) {
-		        csucursalmarca = 1012;
-		        sseriemarca = "AZ";
-		        centidadlegalmarca = 5;
-		        ssucursalmarca = "EMPRESAS AZTECA";
-		    }
+			if (p_razon == 1) {
+				csucursalmarca = 1014;
+				sseriemarca = "AJP";
+				centidadlegalmarca = 7;
+				ssucursalmarca = "EMPRESAS JENNER PRADO";
+			} else if (p_razon == 2) {
+				csucursalmarca = 1015;
+				sseriemarca = "AJL";
+				centidadlegalmarca = 8;
+				ssucursalmarca = "EMPRESAS JENNER LEAN";
+			} else if (p_razon == 3) {
+				csucursalmarca = 1012;
+				sseriemarca = "AZ";
+				centidadlegalmarca = 5;
+				ssucursalmarca = "EMPRESAS AZTECA";
+			}
 		}
 
 		if (pBdefinitivo = true && pMaxamount == -1 && pCconvenio != 309 && pCconvenio != 310 && pCconvenio != 311
@@ -355,80 +357,100 @@ public class CalculosServiceImpl extends JdbcDaoSupport implements CalculosServi
 			log.info("entre global");
 			strsql = "select ufolioactual from c_control_folio where sserie = '" + sseriemarca + "' AND CSUCURSAL = "
 					+ csucursalmarca + " AND CESTADOREGISTRO=31";
-			String vFolioActual = this.getJdbcTemplate().queryForObject(strsql, String.class);
+			Long vFolioActual = this.getJdbcTemplate().queryForObject(strsql, Long.class);
 
+			vFolioActual += 1;
+			BigDecimal subtotal = new BigDecimal(0);
+			BigDecimal iva = new BigDecimal(0);
+			BigDecimal total = new BigDecimal(0);
+			List<String> listFacturaSucursal = new ArrayList<>();
 			for (FuncionFacturacionDto dto : lstCalculos) {
-
-				strsql = "insert into t_factura values(t_factura_sequence.nextval,(select kdatofiscal from c_convenio_dato_fiscal where cconvenio = "
-						+ pCconvenio + ")'||'" + ",'" + ssucursalmarca + "'," + vFolioActual + 1
-						+ ",(select ccliente from c_convenio  where cconvenio ='" + pCconvenio + "')'||',"
-						+ csucursalmarca + ",1,'" + "||'||sum (msubtotal)||'" + "||',0.00,0.00,'"
-						+ "||'||sum (miva)||'','''" + "||'||sum (mtotal)||'''" + "||',1, " + pCconvenio
-						+ ", ' ', '  ', " + centidadlegalmarca + ",sysdate,33,sysdate," + pUserid + "," + pUserid
-						+ ", ' ' ,' ' ,  ' ', '" + sseriemarca + "');";
-
-				this.getJdbcTemplate().execute(strsql);
-
-				strsql = "update c_control_folio set ufolioactual = " + vFolioActual + 1 + " where csucursal = "
-						+ csucursalmarca + " and cestadoregistro=31 and sserie='" + sseriemarca + "'; ";
-				this.getJdbcTemplate().execute(strsql);
-
-				strsql = "update t_orden_sucursal_fac set kfactura = (select kfactura from t_factura where sserie='"
-						+ sseriemarca + "' and csucursal = " + csucursalmarca + " and ufoliofactura = " + vFolioActual
-						+ 1 + ")" + ", cestadoregistro= 35 where cconvenio = " + pCconvenio
-						+ " and cestadoregistro = 37 and kordensucursalfac in (" + dto.getKordensucursalfac() + ");";
-				this.getJdbcTemplate().execute(strsql);
-
-				strsql = "update t_orden_examen_sucursal_fac set kfactura = (select kfactura from t_factura where sserie='"
-						+ sseriemarca + "' and csucursal = " + csucursalmarca + " and ufoliofactura = " + vFolioActual
-						+ 1 + ") " + "where cestadoregistro <> 43 and kordensucursalfac in (" + dto.getKordensucursalfac()
-						+ ");";
-				this.getJdbcTemplate().execute(strsql);
+				subtotal = subtotal.add(dto.getMsubtotal());
+				iva = iva.add(dto.getMiva());
+				total = total.add(dto.getMtotal());
+				listFacturaSucursal.add(String.valueOf(dto.getKordensucursalfac()));
 
 			}
+			listFacturaSucursal = listFacturaSucursal.stream().distinct().collect(Collectors.toList());
 
-		
+			String inKordensucursaclfac = String.join(",", listFacturaSucursal);
+
+			strsql = "insert into t_factura values(t_factura_sequence.nextval,(select kdatofiscal from c_convenio_dato_fiscal where cconvenio = "
+					+ pCconvenio + ")" + ",'" + ssucursalmarca + "'," + vFolioActual
+					+ ",(select ccliente from c_convenio  where cconvenio =" + pCconvenio + ")," + csucursalmarca
+					+ ",1," + subtotal + ",0.00,0.00," + " " + iva + "," + "" + total + "" + ",1, " + pCconvenio
+					+ ", ' ', '  ', " + centidadlegalmarca + ",sysdate,33,sysdate," + pUserid + "," + pUserid
+					+ ", ' ' ,' ' ,  ' ', '" + sseriemarca + "');";
+
+			// this.getJdbcTemplate().execute(strsql);
+
+			strsql = "update c_control_folio set ufolioactual = " + vFolioActual + " where csucursal = "
+					+ csucursalmarca + " and cestadoregistro=31 and sserie='" + sseriemarca + "'; ";
+			// this.getJdbcTemplate().execute(strsql);
+
+			strsql = "update t_orden_sucursal_fac set kfactura = (select kfactura from t_factura where sserie='"
+					+ sseriemarca + "' and csucursal = " + csucursalmarca + " and ufoliofactura = " + vFolioActual + ")"
+					+ ", cestadoregistro= 35 where cconvenio = " + pCconvenio
+					+ " and cestadoregistro = 37 and kordensucursalfac in (" + inKordensucursaclfac + ");";
+			// this.getJdbcTemplate().execute(strsql);
+
+			strsql = "update t_orden_examen_sucursal_fac set kfactura = (select kfactura from t_factura where sserie='"
+					+ sseriemarca + "' and csucursal = " + csucursalmarca + " and ufoliofactura = " + vFolioActual
+					+ ") " + "where cestadoregistro <> 43 and kordensucursalfac in (" + inKordensucursaclfac + ");";
+			// this.getJdbcTemplate().execute(strsql);
+
 		}
 
 		if (pBdefinitivo = true && pMaxamount > -1 && pCconvenio != 309 && pCconvenio != 310 && pCconvenio != 311
 				&& pCconvenio != 312) {
 			Double macumulador = 0.0;
+			BigDecimal subtotal = new BigDecimal(0);
+			BigDecimal iva = new BigDecimal(0);
+			BigDecimal total = new BigDecimal(0);
+			List<String> listFacturaSucursal = new ArrayList<>();
 			for (FuncionFacturacionDto dto : lstCalculos) {
-				macumulador = macumulador + dto.getMtotal().doubleValue();
 
-				strsql = "select ufolioactual  from c_control_folio where sserie = " + sseriemarca + " AND CSUCURSAL = "
-						+ csucursalmarca + " AND CESTADOREGISTRO=31";
-				Long vFolioActual = this.getJdbcTemplate().queryForObject(strsql, Long.class);
-
-				strsql = "insert into t_factura values(t_factura_sequence.nextval,(select kdatofiscal from c_convenio_dato_fiscal where cconvenio = '"
-						+ pCconvenio + "')'||'" + ",'" + ssucursalmarca + "'," + vFolioActual + 1
-						+ ",(select ccliente from c_convenio  where cconvenio ='" + pCconvenio + "')'||',"
-						+ csucursalmarca + ",1,'''" + "||'||sum (msubtotal)||'''" + "||',0.00,0.00,'''"
-						+ "||'||sum (miva)||'','''" + "||'||sum (mtotal)||'''" + "||',1, " + pCconvenio
-						+ ", ' ', '  ', '" + centidadlegalmarca + "',sysdate,33,sysdate," + pUserid + "," + pUserid
-						+ ", ' ' ,' ' ,  ' ', '" + sseriemarca + "');";
-				this.getJdbcTemplate().execute(strsql);
-
-				strsql = "update c_control_folio set ufolioactual = " + vFolioActual + 1 + " where csucursal = "
-						+ csucursalmarca + " and cestadoregistro=31 and sserie='" + sseriemarca + "';";
-
-				if (macumulador < pMaxamount) {
-					strsql = "update t_orden_sucursal_fac set kfactura = (select kfactura from t_factura where sserie='"
-							+ sseriemarca + "' and csucursal = " + csucursalmarca + " and ufoliofactura = "
-							+ vFolioActual + 1 + ")" + ", cestadoregistro= 35 where cconvenio = '" + pCconvenio
-							+ "'and cestadoregistro = 37 and kordensucursalfac in (" + dto.getKordensucursalfac() + ");";
-					this.getJdbcTemplate().execute(strsql);
-				}
-
-				if (macumulador < pMaxamount) {
-					strsql = "update t_orden_examen_sucursal_fac set kfactura = (select kfactura from t_factura where sserie='"
-							+ sseriemarca + "' and csucursal = " + csucursalmarca + " and ufoliofactura = "
-							+ vFolioActual + 1 + ") " + "where cestadoregistro <> 43 and kordensucursalfac in ('"
-							+ dto.getKordensucursalfac() + "');";
-					this.getJdbcTemplate().execute(strsql);
-				}
+				subtotal = subtotal.add(dto.getMsubtotal());
+				iva = iva.add(dto.getMiva());
+				total = total.add(dto.getMtotal());
+				listFacturaSucursal.add(String.valueOf(dto.getKordensucursalfac()));
+				macumulador += total.doubleValue();
+				if (macumulador < pMaxamount)
+					continue;
+				else
+					break;
 
 			}
+			listFacturaSucursal = listFacturaSucursal.stream().distinct().collect(Collectors.toList());
+
+			String inKordensucursaclfac = String.join(",", listFacturaSucursal);
+
+			strsql = "select ufolioactual  from c_control_folio where sserie = " + sseriemarca + " AND CSUCURSAL = "
+					+ csucursalmarca + " AND CESTADOREGISTRO=31";
+			Long vFolioActual = this.getJdbcTemplate().queryForObject(strsql, Long.class);
+
+			vFolioActual += 1;
+			strsql = "insert into t_factura values(t_factura_sequence.nextval,(select kdatofiscal from c_convenio_dato_fiscal where cconvenio = '"
+					+ pCconvenio + "')'||'" + ",'" + ssucursalmarca + "'," + vFolioActual
+					+ ",(select ccliente from c_convenio  where cconvenio ='" + pCconvenio + "')'||'," + csucursalmarca
+					+ ",1,'''" + "||'||sum (msubtotal)||'''" + "||',0.00,0.00,'''" + "||'||sum (miva)||'','''"
+					+ "||'||sum (mtotal)||'''" + "||',1, " + pCconvenio + ", ' ', '  ', '" + centidadlegalmarca
+					+ "',sysdate,33,sysdate," + pUserid + "," + pUserid + ", ' ' ,' ' ,  ' ', '" + sseriemarca + "');";
+			this.getJdbcTemplate().execute(strsql);
+
+			strsql = "update c_control_folio set ufolioactual = " + vFolioActual + " where csucursal = "
+					+ csucursalmarca + " and cestadoregistro=31 and sserie='" + sseriemarca + "';";
+
+			strsql = "update t_orden_sucursal_fac set kfactura = (select kfactura from t_factura where sserie='"
+					+ sseriemarca + "' and csucursal = " + csucursalmarca + " and ufoliofactura = " + vFolioActual + ")"
+					+ ", cestadoregistro= 35 where cconvenio = '" + pCconvenio
+					+ "'and cestadoregistro = 37 and kordensucursalfac in (" + inKordensucursaclfac + ");";
+			this.getJdbcTemplate().execute(strsql);
+
+			strsql = "update t_orden_examen_sucursal_fac set kfactura = (select kfactura from t_factura where sserie='"
+					+ sseriemarca + "' and csucursal = " + csucursalmarca + " and ufoliofactura = " + vFolioActual
+					+ ") " + "where cestadoregistro <> 43 and kordensucursalfac in ('" + inKordensucursaclfac + "');";
+			this.getJdbcTemplate().execute(strsql);
 
 		}
 
